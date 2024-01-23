d06a2371e98536299d42e21f016d2872abab83a0
matheus ferreira <mat.matheusf@hotmail.com>
2024-01-23 10:20:13 -0300

    correcao de variaveis no server

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const Twitter = require('twitter-lite');
 
 //TESTANDO GLITCH
 // Insira suas chaves de acesso aqui
-const api_key = " " 
-const api_key_secret = " " 
-const access_token = " " 
-const access_token_secret = " " 
+const api_key = "ChjC6gORvV6BKqk9X0TVZvd9a" 
+const api_key_secret = "HSrtPQLHHXdzbXHYniYuU96HYugBu7pangbz6LvKPuoi8aYRbH" 
+const access_token = "1651597515827945476-AlLOzQtVoler20eNG4Z9dSZwZ03Wng" 
+const access_token_secret = "gdgq42Jn8EpLPYAlPtIBySV1dFNg6X36MBuU7AU8Spb81" 
 
 
 const client = new Twitter({    
- api_key,
- api_key_secret,
- access_token_key: access_token,
- access_token_secret,
+   consumer_key: api_key,
+   consumer_secret: api_key_secret,
+   access_token_key: access_token,
+   access_token_secret,
 });
 
 
